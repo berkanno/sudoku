a301c0ad6a0237daec736b8d5d618abcd1f70601
Berkan <berkanoral25@gmail.com>
2024-07-19 10:33:49 +0300

    vuetfit.ts upd

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,11 +9,65 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import 'vuetify/styles';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+const customTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    surface: "#FFFFFF",
+    primary: "#5C6BC0",
+    "primary-darken-1": "#3F51B5",
+    secondary: "#26A69A",
+    "secondary-darken-1": "#00796B",
+    error: "#D32F2F",
+    info: "#29B6F6",
+    success: "#66BB6A",
+    warning: "#FFA726",
+    background: "#F5F5F5",
+    topMenuColor: "#4497a8",
+    subMenuColor: "#ECEFF1",
+    profileCardColor: "#FF7043",
+    lastOrdersCardColor: "#FF7043",
+    niceFontBlakc: "#0A0A0A",
+    black: "#000000",
+    darkPurple: "#762d7e",
+    secondarymenu: "#4497a8",
+  },
+}
+
 export default createVuetify({
+  components,
+  directives,
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: "customTheme",
+    themes: {
+      customTheme,
+    },
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  defaults: {
+    VCard: {
+      variant: "flat",
+      border: true,
+      color: 'white',
+    },
+    VAppBar: {
+      color: 'white'
+    },
+    VToolbar: {
+      color: 'white'
+    },
   },
 })

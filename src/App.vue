ed9276a4f3183f08525e151a18035ff717e8f430
Berkan <berkanoral25@gmail.com>
2024-07-19 10:35:58 +0300

    pinia added

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   <v-app>
     <v-main>
       <v-card class="fill-height" >
-        Sudoku Project
+        <v-card-text>
+          <v-row>
+            <v-col>
+
+            </v-col>
+          </v-row>
+        </v-card-text>
       </v-card>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  //
+  
 </script>

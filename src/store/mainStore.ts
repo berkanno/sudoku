5f95cc7f3e668d25aaa77695a9d695109f538e11
Berkan <berkanoral25@gmail.com>
2024-07-19 17:20:35 +0300

    jhgjhgj

--- src/store/mainStore.ts
+++ src/store/mainStore.ts
@@ -5,11 +5,13 @@ export const useMainStore = defineStore("mainStore", () => {
 
     // const sudokuList = ref([] as number[])
     
-    const createSudokuList= async () => {
+    const createSudokuList= () => {
       let sudokuList = [] as number[][]
-      for (let y = 0; y < 6; y++) {
+      for (let y = 0; y < 3; y++) {
         sudokuList[y] = []
         const verticalValue = (Math.ceil((y + 1) / 3) - 1)
+        console.log(y,verticalValue,"YYYYYYYYYYYYYYYYYYYYYYYYYY")
+        sleep(1000)
         for (let x = 0; x < 9; x++) {
             const startSliceValue = (Math.ceil((x + 1)  / 3) - 1) * 3
             let number = 1
@@ -18,58 +20,26 @@ export const useMainStore = defineStore("mainStore", () => {
                 number = randomCreateNumberBetweenOneAndNine()
                 isWhileOut = sudokuList[y].includes(number)
                 if(y > 0 && !isWhileOut) {
-                    for (let z = 1; z < (y + 1); z++) {
-                        console.log(z, y,startSliceValue,sudokuList[y-z].slice(startSliceValue,startSliceValue + 3),number,"JDFKGLDFLGMLKDNLDKGM")
-                       if(sudokuList[y-z].slice(startSliceValue,startSliceValue + 3).includes(number) && !isWhileOut && verticalValue === 0){
+                    for (let z = y; -1 < z; z--) {
+                        console.log(z, y,(y === 2 && (sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number) || sudokuList[1].slice(startSliceValue,startSliceValue + 3).includes(number))),number,"JDFKGLDFLGMLKDNLDKGM")
+                       if(((y === 2 && (sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number) || sudokuList[1].slice(startSliceValue,startSliceValue + 3).includes(number))) || (y === 1 && sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number))) && !isWhileOut && verticalValue === 0){
                          isWhileOut = true
-                        } else if (verticalValue === 1 && !isWhileOut) {
-                          console.log("",y,"sdfsdfsdfsdfsdfsdfsdfdsfdsfsdf")
+                        } else if (verticalValue === 1 && !isWhileOut && (sudokuList[y - z][x] === number || (y === 4 && sudokuList[3].slice(startSliceValue,startSliceValue + 3).includes(number)) || (y === 5 && (sudokuList[4].slice(startSliceValue,startSliceValue + 3).includes(number) || sudokuList[3].slice(startSliceValue,startSliceValue + 3).includes(number))))) {
+                            isWhileOut = true
                         }
                     }
                 }
             }
             sudokuList[y].push(number)
         }
-        console.log(verticalValue,"verticalValue")
+       
       }
-            // sudokuList[0] = []
-            // for (let x = 0; x < 9; x++) {
-            //     let number = 1
-            //     do {
-            //         number = await randomCreateNumberBetweenOneAndNine()
-            //     }
-            //     while(sudokuList[0].includes(number))
-            //     sudokuList[0].push(number)
-            // }
-
-            // sudokuList[1] = []
-            // for (let x = 0; x < 9; x++) {
-            //     const startSliceValue = (Math.ceil((x + 1)  / 3) - 1) * 3
-            //     let number = 1
-            //     let isWhileOut = false
-            //     do {
-            //         number = await randomCreateNumberBetweenOneAndNine()
-            //         isWhileOut = sudokuList[1].includes(number) || sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number) 
-            //     }
-            //     while(isWhileOut)
-            //     sudokuList[1].push(number)
-            // }
-
-            // sudokuList[2] = []
-            // for (let x = 0; x < 9; x++) {
-            //     const startSliceValue = (Math.ceil((x + 1)  / 3) - 1) * 3
-            //     let number = 1
-            //     let isWhileOut = false
-            //     do {
-            //         number = await randomCreateNumberBetweenOneAndNine()
-            //         isWhileOut = sudokuList[2].includes(number) || sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number) || sudokuList[1].slice(startSliceValue,startSliceValue + 3).includes(number) 
-            //     }
-            //     while(isWhileOut)
-            //     sudokuList[2].push(number)
-            // }
-
-
         console.log(sudokuList,"asdada")
+    }
+
+    const sleep = (delay:number) => {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay);
     }
 
     const randomCreateNumberBetweenOneAndNine = () => {

f4934488a9825ca5e6445952995f01493b5218df
Berkan <berkanoral25@gmail.com>
2024-07-19 10:31:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-card class="fill-height" >
+        Sudoku Project
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  //
+</script>

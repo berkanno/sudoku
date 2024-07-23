3de7131db2f295da37e96257b793074c13b0f818
Berkan oral <berkanoral25@gmail.com>
2024-07-23 20:13:13 +0300

    created sudoku create table function

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <v-app>
     <v-main>
-      <v-card class="fill-height" >
+      <v-card class="fill-height">
         <v-card-text>
-          <v-row class="flex-column" >
+          <v-row class="flex-column">
             <v-col>
-               <v-btn @click="createSudokuList()" >Başla</v-btn>
+              <v-btn @click="createSudokuList()">Başla</v-btn>
             </v-col>
             <v-col>
-               <v-btn>Sıfırla</v-btn>
+              <v-btn>Sıfırla</v-btn>
             </v-col>
+            <!-- <v-col>
+            {{ sudokuList }}
+            </v-col> -->
             <v-col>
-
+              <v-row v-for="number in sudokuList" :key="number">
+                <v-col v-for="i in 10" :key="i">
+                  {{ number[i-1] }}
+                </v-col>
+              </v-row>
             </v-col>
           </v-row>
         </v-card-text>
@@ -21,7 +28,10 @@
 </template>
 
 <script setup lang="ts">
-import { useMainStore } from "@/store/mainStore"
-const mainStore = useMainStore()
-const { createSudokuList } = mainStore
+import { useMainStore } from "@/store/mainStore";
+import { storeToRefs } from "pinia";
+
+const mainStore = useMainStore();
+const { createSudokuList } = mainStore;
+const { sudokuList } = storeToRefs(mainStore);
 </script>
--- src/store/mainStore.ts
+++ src/store/mainStore.ts
@@ -2,53 +2,41 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useMainStore = defineStore("mainStore", () => {
+  const sudokuList = ref([] as number[][])
 
-    // const sudokuList = ref([] as number[])
-    
-    const createSudokuList= () => {
-      let sudokuList = [] as number[][]
-      for (let y = 0; y < 3; y++) {
-        sudokuList[y] = []
-        const verticalValue = (Math.ceil((y + 1) / 3) - 1)
-        console.log(y,verticalValue,"YYYYYYYYYYYYYYYYYYYYYYYYYY")
-        sleep(1000)
-        for (let x = 0; x < 9; x++) {
-            const startSliceValue = (Math.ceil((x + 1)  / 3) - 1) * 3
-            let number = 1
-            let isWhileOut = true
-            while(isWhileOut) {
-                number = randomCreateNumberBetweenOneAndNine()
-                isWhileOut = sudokuList[y].includes(number)
-                if(y > 0 && !isWhileOut) {
-                    for (let z = y; -1 < z; z--) {
-                        console.log(z, y,(y === 2 && (sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number) || sudokuList[1].slice(startSliceValue,startSliceValue + 3).includes(number))),number,"JDFKGLDFLGMLKDNLDKGM")
-                       if(((y === 2 && (sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number) || sudokuList[1].slice(startSliceValue,startSliceValue + 3).includes(number))) || (y === 1 && sudokuList[0].slice(startSliceValue,startSliceValue + 3).includes(number))) && !isWhileOut && verticalValue === 0){
-                         isWhileOut = true
-                        } else if (verticalValue === 1 && !isWhileOut && (sudokuList[y - z][x] === number || (y === 4 && sudokuList[3].slice(startSliceValue,startSliceValue + 3).includes(number)) || (y === 5 && (sudokuList[4].slice(startSliceValue,startSliceValue + 3).includes(number) || sudokuList[3].slice(startSliceValue,startSliceValue + 3).includes(number))))) {
-                            isWhileOut = true
-                        }
-                    }
-                }
-            }
-            sudokuList[y].push(number)
+  const createSudokuList = () => {
+    sudokuList.value = [];
+    let number = 1;
+    [0,1,2,3,4,5,6,7,8].forEach(y => {
+      sudokuList.value[y] = [];
+      for (let x = 0; x < 9; x++) {
+        const startSliceValue = (Math.ceil((x + 1) / 3) - 1) * 3;
+        if([0,1,2].includes(y)) {
+          number = createNumber([...(sudokuList.value[y] ?? []), ...([1,2].includes(y) ? [...sudokuList.value[0].slice(startSliceValue,startSliceValue+3)] : []), ...([2].includes(y) ? [...sudokuList.value[1].slice(startSliceValue,startSliceValue+3)] : [])])
+          // number = createNumber([...(sudokuList.value[y] ?? [])])
+          sudokuList.value[y].push(number);
+        } else if ([3,4,5].includes(y)) {
+          number = createNumber([...[sudokuList.value[0][x],sudokuList.value[1][x],sudokuList.value[2][x]],...(sudokuList.value[y] ?? []), ...([4,5].includes(y) ? [...sudokuList.value[3].slice(startSliceValue,startSliceValue+3)] : []), ...([5].includes(y) ? [...sudokuList.value[4].slice(startSliceValue,startSliceValue+3)] : [])])
+          sudokuList.value[y].push(number);
+        } else if ([6,7,8].includes(y)) {
+          number = createNumber([...[sudokuList.value[0][x],sudokuList.value[1][x],sudokuList.value[2][x],sudokuList.value[3][x],sudokuList.value[4][x],sudokuList.value[5][x]],...(sudokuList.value[y] ?? []), ...([7,8].includes(y) ? [...sudokuList.value[6].slice(startSliceValue,startSliceValue+3)] : []), ...([8].includes(y) ? [...sudokuList.value[7].slice(startSliceValue,startSliceValue+3)] : [])])
+          sudokuList.value[y].push(number);
         }
-       
       }
-        console.log(sudokuList,"asdada")
-    }
+    })
+    if(sudokuList.value.some(e => e.includes(undefined))) createSudokuList()
+  };
 
-    const sleep = (delay:number) => {
-        var start = new Date().getTime();
-        while (new Date().getTime() < start + delay);
-    }
+  const sleep = (delay: number) => {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+  };
+  const createNumber = (excludeList?: number[]) => {
+    let randomSpecificNumberList = [1,2,3,4,5,6,7,8,9]
+    randomSpecificNumberList = randomSpecificNumberList.filter((e: number) => !(excludeList ?? []).filter(element => element != undefined).includes(e))
+    const findNumberIndexRandom = randomSpecificNumberList.length === 1 ? 0 : Math.floor(Math.random() * (randomSpecificNumberList.length - 1 + 1)) + 0
+    return randomSpecificNumberList[findNumberIndexRandom]
+  }
 
-    const randomCreateNumberBetweenOneAndNine = () => {
-        let number = Number(Math.floor(Math.random()*10))
-        while (number === 0) {
-            number = Number(Math.floor(Math.random()*10))
-        }
-        return number
-    }
-
-    return { createSudokuList }
-})
+  return { createSudokuList, sudokuList };
+});
